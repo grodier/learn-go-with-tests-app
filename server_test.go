a6508cf4d783e1e5ecd224c0666457aa91214fc9
grodier <grodier3@gmail.com>
2024-06-09 12:15:43 -0400

    Refactor parsing league

--- server_test.go
+++ server_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -124,7 +123,7 @@ func TestLeague(t *testing.T) {
 
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	t.Helper()
-	err := json.NewDecoder(body).Decode(&league)
+	league, err := NewLeague(body)
 
 	if err != nil {
 		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
