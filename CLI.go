2ddf1e8a29e2121772cb3a7f3054a55b5974b569
grodier <grodier3@gmail.com>
2024-06-12 23:23:42 -0400

    Add ability to read from bufio scanner and test win supported with multiple different users in cli

--- CLI.go
+++ CLI.go
@@ -1,6 +1,10 @@
 package poker
 
-import "io"
+import (
+	"bufio"
+	"io"
+	"strings"
+)
 
 type CLI struct {
 	playerStore PlayerStore
@@ -8,5 +12,11 @@ type CLI struct {
 }
 
 func (c *CLI) PlayPoker() {
-	c.playerStore.RecordWin("Chris")
+	reader := bufio.NewScanner(c.in)
+	reader.Scan()
+	c.playerStore.RecordWin(extractWinner(reader.Text()))
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
 }
