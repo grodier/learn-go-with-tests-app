afa5c8a98e7373159ff5e80d4696bcbc8ec76dfd
grodier <grodier3@gmail.com>
2024-06-12 23:16:52 -0400

    Refactor test assert

--- server_test.go
+++ server_test.go
@@ -86,14 +86,7 @@ func TestStoreWins(t *testing.T) {
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
-
-		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-		}
-
-		if store.winCalls[0] != player {
-			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
-		}
+		assertPlayerWin(t, &store, player)
 	})
 }
 
@@ -130,6 +123,16 @@ func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	}
 
 	return
+}
+
+func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
+	if len(store.winCalls) != 1 {
+		t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	}
+
+	if store.winCalls[0] != winner {
+		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
+	}
 }
 
 func assertLeague(t testing.TB, got, want []Player) {
