78bbee6f522d7cabbdbb77261ce2db7be5b2c4e9
grodier <grodier3@gmail.com>
2024-06-13 22:54:54 -0400

    Integrate with main minor refactors

--- CLI.go
+++ CLI.go
@@ -8,15 +8,26 @@ import (
 
 type CLI struct {
 	playerStore PlayerStore
-	in          io.Reader
+	in          *bufio.Scanner
+}
+
+func NewCLI(store PlayerStore, in io.Reader) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+	}
 }
 
 func (c *CLI) PlayPoker() {
-	reader := bufio.NewScanner(c.in)
-	reader.Scan()
-	c.playerStore.RecordWin(extractWinner(reader.Text()))
+	userInput := c.readLine()
+	c.playerStore.RecordWin(extractWinner(userInput))
 }
 
 func extractWinner(userInput string) string {
 	return strings.Replace(userInput, " wins", "", 1)
+}
+
+func (c *CLI) readLine() string {
+	c.in.Scan()
+	return c.in.Text()
 }
--- server_test.go
+++ server_test.go
@@ -9,25 +9,6 @@ import (
 	"testing"
 )
 
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   []Player
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
-
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{
@@ -86,7 +67,7 @@ func TestStoreWins(t *testing.T) {
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
-		assertPlayerWin(t, &store, player)
+		AssertPlayerWin(t, &store, player)
 	})
 }
 
@@ -123,16 +104,6 @@ func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	}
 
 	return
-}
-
-func assertPlayerWin(t testing.TB, store *StubPlayerStore, winner string) {
-	if len(store.winCalls) != 1 {
-		t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
-	}
-
-	if store.winCalls[0] != winner {
-		t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], winner)
-	}
 }
 
 func assertLeague(t testing.TB, got, want []Player) {
