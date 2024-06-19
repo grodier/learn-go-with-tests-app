927bc4255983b87408483797373f343c45211dc5
grodier <grodier3@gmail.com>
2024-06-19 12:26:52 -0400

    Initial setup of game endpoint on server

--- server_test.go
+++ server_test.go
@@ -95,6 +95,19 @@ func TestLeague(t *testing.T) {
 	})
 }
 
+func TestGame(t *testing.T) {
+	t.Run("GET /game returns 200", func(t *testing.T) {
+		server := NewPlayerServer(&StubPlayerStore{})
+
+		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func getLeagueFromResponse(t testing.TB, body io.Reader) (league []Player) {
 	t.Helper()
 	league, err := NewLeague(body)
